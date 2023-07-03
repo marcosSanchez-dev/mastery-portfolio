1d0a49832930ce0e17d3896fd7b18d60da1be300
Marcos <marcos.sanchez@saei.mx>
2023-07-03 17:35:56 -0500

    Loading tron model

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,17 +4,25 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = () => {
-  const computer = useGLTF("./desktop_pc/scene.gltf");
+  const computer = useGLTF("./tron_character/scene.gltf");
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black" />
+      <hemisphereLight intensity={1} groundColor="black" />
       <pointLight intensity={1} />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
       <primitive
         object={computer.scene}
-        scale={1}
+        scale={0.005}
         position={[0, -3.25, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
+        // rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
   );
@@ -29,7 +37,11 @@ const ComputersCanvas = () => {
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls enableZoom={false} />
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2.5}
+          minPolarAngle={Math.PI / 2.5}
+        />
         <Computers />
       </Suspense>
       <Preload all />

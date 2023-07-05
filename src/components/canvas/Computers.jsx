6f4f563516b9612732de75f5e2b983dbdd0c61fd
Marcos Sanchez <msanchez@hircasa.com>
2023-07-05 12:42:13 -0600

    no bugs

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -55,15 +55,15 @@ const ComputersCanvas = () => {
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
-      <Suspense>
-        {/* <OrbitControls
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
-        /> */}
+        />
         <Computers isMobile={isMobile} />
       </Suspense>
-      {/* <Preload all /> */}
+      <Preload all />
     </Canvas>
   );
 };

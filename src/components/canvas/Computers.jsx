a32e66e1c2c81418e8ebf82bae4f5920b9e7d9b6
Marcos Sanchez <msanchez@hircasa.com>
2023-07-13 01:26:14 -0600

    positioting tron 1

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,12 +1,33 @@
 import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import { Float, OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
   const computer = useGLTF("./tron_character/scene.gltf");
 
-  return (
+  return !isMobile ? (
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+      <mesh>
+        <hemisphereLight intensity={1} groundColor="black" />
+        <pointLight intensity={1} />
+        <spotLight
+          position={[-20, 50, 10]}
+          angle={0.12}
+          penumbra={1}
+          intensity={1}
+          castShadow
+          shadow-mapSize={1024}
+        />
+        <primitive
+          object={computer.scene}
+          scale={isMobile ? 0.003 : 0.002}
+          position={[0, -3.25, -0.2]}
+          rotation={[0, 1.3, 0]}
+        />
+      </mesh>
+    </Float>
+  ) : (
     <mesh>
       <hemisphereLight intensity={1} groundColor="black" />
       <pointLight intensity={1} />
@@ -20,7 +41,7 @@ const Computers = ({ isMobile }) => {
       />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.003 : 0.005}
+        scale={isMobile ? 0.003 : 0.002}
         position={[0, -3.25, -1.5]}
         rotation={[0, 1.3, 0]}
       />
@@ -56,11 +77,14 @@ const ComputersCanvas = () => {
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls
-          enableZoom={false}
-          maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2}
-        />
+        {!isMobile ? (
+          <OrbitControls
+            enableZoom={false}
+            maxPolarAngle={Math.PI / 2}
+            minPolarAngle={Math.PI / 2}
+          />
+        ) : null}
+
         <Computers isMobile={isMobile} />
       </Suspense>
       <Preload all />
